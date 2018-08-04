95439ca6630a9852018640c0faf013a47242794f
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 21:29:49 +0530

    Update TestDateAddDays.java

--- DateTimeAPI/com/datetime/api/TestDateAddDays.java
+++ DateTimeAPI/com/datetime/api/TestDateAddDays.java
@@ -11,12 +11,15 @@ public class TestDateAddDays {
 		System.out.println("Enter the date as DD/MM/YYYY");
 		
 		String str = scanner.nextLine();
-		String[] date = str.split("/");
+		String[] date = str.split("/"); //splitiing the date on every occurance of'/' 
+		
 		
 		DateAddDays day = new DateAddDays(Integer.parseInt(date[0]),Integer.parseInt(date[1]),Integer.parseInt(date[2]));
-		System.out.println("Number of Days to be added");
 		
+		System.out.println("Number of Days to be added");
 		int num = scanner.nextInt();
+		
+		//adding days to the date stored in 'day'
 		LocalDate newDate = day.AddDays(num);
 		
 		System.out.println(newDate.getDayOfMonth()+"/"+newDate.getMonthValue()+"/"+newDate.getYear());
