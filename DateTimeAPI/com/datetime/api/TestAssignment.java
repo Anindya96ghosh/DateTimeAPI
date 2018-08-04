81c6ea0b85ed042a7d84b6fb43e1cc1139d49594
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 21:25:59 +0530

    Update TestAssignment.java

--- DateTimeAPI/com/datetime/api/TestAssignment.java
+++ DateTimeAPI/com/datetime/api/TestAssignment.java
@@ -9,10 +9,14 @@ public class TestAssignment {
 
 	public static void main(String[] args) {
 		
+		
+		//Creating an arraylist
 		ArrayList<Assignment> arrayList = new ArrayList<>();
 		
+		//creating object array
 		Assignment[] assignment = new Assignment[5];
 
+		//assigning dates for the assignments
 		LocalDate date;
 		date= LocalDate.of(2018, 8, 20);
 		
@@ -30,19 +34,26 @@ public class TestAssignment {
 		date= LocalDate.of(2018, 9, 15);
 		assignment[4] = new Assignment("DWM",date);
 		
+		//adding to the array list
 		for(int i =0; i< 5; i++)
 			arrayList.add(assignment[i]);
 		
+		//displaying the list
 		arrayList.stream().forEach(System.out::println);
 		
+		//removing from the list
 		arrayList.remove(assignment[2]);
 		System.out.println("=======================================================================");
+		//printing the remaining list
 		arrayList.stream().forEach(System.out::println);
 	
 		System.out.println("=======================================================================");
 		
+		//sorting the list based on the due date
 		Comparator<Assignment> comparator = (arg0, arg1) -> arg0.getDueDate().compareTo(arg1.getDueDate());
 		Collections.sort(arrayList, comparator);
+		
+		//printing the first element of the list
 		System.out.println(arrayList.get(0));
 	}
 
