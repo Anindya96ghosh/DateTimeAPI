b8959fef8225de462561df8a71f63015bcff8849
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 21:10:07 +0530

    Update DateAddDays.java

--- DateTimeAPI/com/datetime/api/DateAddDays.java
+++ DateTimeAPI/com/datetime/api/DateAddDays.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 
 public class DateAddDays {
 	LocalDate date;
-
+	
+	//method to create date by the use of LocalDate.of
 	public DateAddDays(int day, int month, int yyyy) {
 		date = LocalDate.of(yyyy,month,day);
 	}
 	
+	//method to add days
 	public LocalDate AddDays(int num) {
 		LocalDate updated = date.plusDays(num);
 		return updated;
