1b230e4d8227da42c900425808067ec23219147b
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 18:32:08 +0530

    Add files via upload

---
+++ DateTimeAPI/com/datetime/api/DisplayToday.java
@@ -0,0 +1,13 @@
+package com.datetime.api;
+
+import java.time.LocalDate;
+
+public class DisplayToday {
+	
+	public static void main(String args[]) {
+		LocalDate date;
+		
+		date = LocalDate.now();
+		System.out.println(date.getDayOfWeek()+", "+date.getMonth()+" "+date.getDayOfMonth()+", "+date.getYear());
+	}
+}
---
+++ DateTimeAPI/com/datetime/api/TestDateAddDays.java
@@ -0,0 +1,25 @@
+package com.datetime.api;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class TestDateAddDays {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the date as DD/MM/YYYY");
+		
+		String str = scanner.nextLine();
+		String[] date = str.split("/");
+		
+		DateAddDays day = new DateAddDays(Integer.parseInt(date[0]),Integer.parseInt(date[1]),Integer.parseInt(date[2]));
+		System.out.println("Number of Days to be added");
+		
+		int num = scanner.nextInt();
+		LocalDate newDate = day.AddDays(num);
+		
+		System.out.println(newDate.getDayOfMonth()+"/"+newDate.getMonthValue()+"/"+newDate.getYear());
+	}
+
+}
